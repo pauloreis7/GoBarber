d6dd39e6fb65bbc817765eba7bd4bdda137ef1aa
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2021-02-24 15:51:26 -0300

    Aprimorando a usabilidade do layout

--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -1,11 +1,15 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   color: #666360;
-
+  border: 2px solid #232129;
   padding-left: 16px;
   width: 100%;
 
@@ -28,8 +32,23 @@ export const Container = styled.div`
     }
   }
 
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border: 2px solid #ff9000;
+    `
+  }
+
   svg {
     margin-right: 16px;
+
+    ${props =>
+      props.isFilled &&
+      css`
+        color: #ff9000;
+      `
+    }
   }
 
 `;
