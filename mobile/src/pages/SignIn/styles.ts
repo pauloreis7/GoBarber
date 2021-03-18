4b563c75bf5fe42f2e2ada1321e5bda9b999b95a
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2021-03-18 16:44:51 -0300

    Componentes reutilizáveis criados

--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -3,7 +3,8 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
     flex: 1;
     justify-content: center;
-    align-items: center
+    align-items: center;
+    padding: 0 30px;
 `;
 
 export const Title = styled.Text`
