00a8ce32a612b34f4c2ca340cf6e1445e29a394b
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2021-03-21 22:03:39 -0300

    Construindo as telas da aplicação e trabalhando com formulários

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useCallback, useRef } from 'react';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
-import logoImg from '../../assets/logo.png';
+import logoImg from '../../assets/logo.png'
 
-import { Container, Title } from './styles';
+import { 
+    Container,
+    Title,
+    ForgotPassword,
+    ForgotPasswordText,
+    CreateAccountButton,
+    CreateAccountButtonText 
+} from './styles';
 
 const SignIn: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
+    const navigate = useNavigation();
+
+    const handleSignIn = useCallback((data: object) => {
+
+    }, [])
+
     return (
-        <Container>
-            <Image source={logoImg} />
+        <>
+            <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined }
+                enabled
+            >
+                <ScrollView
+                    contentContainerStyle={{ flex: 1 }}
+                    keyboardShouldPersistTaps="handled"
+                >
+                    <Container>
+                        <Image source={logoImg} />
 
-            <Title>Faça seu Login</Title>
+                        <View>
+                            <Title>Faça seu Login</Title>
+                        </View>
 
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
+                        <Form ref={formRef} onSubmit={handleSignIn}>
 
-            <Button onPress={() => {console.log("Foi")}}>Entrar</Button>
+                            <Input name="email" icon="mail" placeholder="E-mail" />
+                            
+                            <Input name="password" icon="lock" placeholder="Senha" />
 
-        </Container>
+                            <Button onPress={() => formRef.current?.submitForm()}>Entrar</Button>
+
+                        </Form>
+
+                        <ForgotPassword onPress={() => {}}>
+                            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+                        </ForgotPassword>
+
+                    </Container>
+                </ScrollView>    
+            </KeyboardAvoidingView>
+
+            <CreateAccountButton onPress={() => navigate.navigate('SignUp')}>
+                <Icon name="log-in" size={20} color="#ff9000" />
+                <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+            </CreateAccountButton>
+        </>
     );
 }
 
