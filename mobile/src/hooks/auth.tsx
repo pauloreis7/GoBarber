a699b02129edb0e2f9c666c5c2fec52fb29f6be4
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2021-03-25 19:55:03 -0300

    Criando lógica das rotas privadas

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -15,6 +15,7 @@ interface SingInCreadentials {
 
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credentials: SingInCreadentials): Promise<void>;
   singOut(): void;
 }
@@ -23,6 +24,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStorageData() {
@@ -31,6 +33,8 @@ const AuthProvider: React.FC = ({ children }) => {
       if(token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) })
       } 
+
+      setLoading(false)
     }
 
     loadStorageData();
@@ -60,7 +64,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, [])
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, singOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, singOut }}>
       {children}
     </AuthContext.Provider>
   );
