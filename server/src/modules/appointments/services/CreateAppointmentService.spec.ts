30c4de0a22b349deb0d69196ef0f48992abf5948
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2021-04-05 22:42:50 -0300

    Implementando testes automatizados

---
+++ server/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
+import CreateAppointmentService from './CreateAppointmentService'
+
+describe("CreateAppointment", () => {
+  it("should be able to create a new appointment", async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(fakeAppointmentsRepository);
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '1234567'
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1234567');
+  })
+
+  it("should not be able to create a new appointment in same time", async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(fakeAppointmentsRepository);
+
+    const appointmentDate = new Date();
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '1234567'
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '1234567'
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  })
+})
